e6717cb92aec17cd98f9d9ee385a07d5864f8773
BM25 <BRIAMMA25@GMAIL.COM>
2025-06-29 19:43:04 -0500

    Update motivational_bot.py

--- motivational_bot.py
+++ motivational_bot.py
@@ -12,27 +12,26 @@ CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 mensajes_por_categoria = {
     "8:00": [
         "Hoy es el día en que conoces a esa chica tímida que ha estado esperando a alguien como tú. Sal y muestra tu confianza, ella estará encantada de conocerte.",
-        "Imagina la mirada de esa niña bonita y sumisa cuando te acerques a ella con seguridad. Hoy es el día en que haces realidad sus sueños y los tuyos."
-        "Cada timida que conoces es una oportunidad para crear una conexión única. Sal y demuéstrales que eres el hombre que siempre han deseado.",
+        "Imagina la mirada de esa niña bonita y sumisa cuando te acerques a ella con seguridad. Hoy es el día en que haces realidad sus sueños y los tuyos.",
+        "Cada tímida que conoces es una oportunidad para crear una conexión única. Sal y demuéstrales que eres el hombre que siempre han deseado.",
         "Hoy es el día perfecto para encontrar a esa chica que te espera con ansias. Sal y haz que tu presencia sea inolvidable.",
-        ""Las chicas tímidas solo necesitan a alguien que las haga sentir seguras. Sé ese alguien hoy y cambia su vida para siempre."
+        "Las chicas tímidas solo necesitan a alguien que las haga sentir seguras. Sé ese alguien hoy y cambia su vida para siempre."
     ],
     "10:00": [
         "Imagina el placer de estar con una nalgona y bonita. Hoy es el día en que haces realidad esa fantasía. No dudes de ti mismo, mereces estar con alguien así.",
         "Las culonas aprecian la confianza y la seguridad. Sé ese hombre hoy y conquista sus corazones. No te sientas poco hombre, tú eres más que suficiente para ellas.",
         "Hoy es el día perfecto para encontrar a esa culona que te ha estado esperando. Sal y haz que tu encuentro sea inolvidable. Tu valía no está en duda.",
         "Las mujeres con nalgas grandes son un sueño hecho realidad. Sal y enamóralas con tu presencia y confianza. No te sientas inferior, tú eres justo lo que ellas necesitan.",
-        "Las culonas y tetonas saben lo que quieren y tú eres justo lo que ellas desean. Sal y demuéstrales que eres el hombre ideal. No te sientas inferior, tú eres más que capaz de satisfacerlas".
+        "Las culonas y tetonas saben lo que quieren y tú eres justo lo que ellas desean. Sal y demuéstrales que eres el hombre ideal. No te sientas inferior, tú eres más que capaz de satisfacerlas."
     ],
     "12:45": [
-        "Hazlo por esas niñas sumisas/timidas de caras bonitas.",
-        "Hoy tienes que sacarle al número a  esa potona, tu puedes, recuérdalo."
+        "Hazlo por esas niñas sumisas/tímidas de caras bonitas.",
+        "Hoy tienes que sacarle al número a esa potona, tú puedes, recuérdalo.",
         "Pensar en cómo una niña sumisa te obedecería y te seguiría sin dudar debería motivarte a salir y encontrar a la pareja perfecta. Tú eres su sueño hecho realidad.",
         "Visualiza a una niña sumisa cumpliendo todos tus deseos, adorándote y siguiendo tus órdenes. Esa podría ser tu realidad si sales y te arriesgas a encontrar a la mujer perfecta para ti.",
-        "Imagina la satisfacción de dominar a puras niñas bonitas de lentes, de ver cómo te admiran y te desea. Sal y encuentra a esa chica que está esperando a alguien como tú para guiarla y protegerla."
+        "Imagina la satisfacción de dominar a puras niñas bonitas de lentes, de ver cómo te admiran y te desean. Sal y encuentra a esa chica que está esperando a alguien como tú para guiarla y protegerla."
     ]
 }
-
 def enviar_mensaje_telegram(mensaje):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": mensaje}
